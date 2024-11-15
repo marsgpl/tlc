e2f6f179a5d6074022b7c25c350d5da8ee6a3327
Iurii Belobeev <marsgpl@gmail.com>
2024-11-16 00:11:47 +0500

    more types

--- src/parser.c
+++ src/parser.c
@@ -18,7 +18,15 @@ static void error_unexpected_symbol(p *parser, uc c) {
         NULL,
         "token",
         "word",
-        "number",
+        "NUM_0",
+        "NUM_DIG",
+        "NUM_DOT",
+        "NUM_X",
+        "NUM_E",
+        "NUM_X_DIG",
+        "NUM_X_DOT",
+        "NUM_E_DIG",
+        "NUM_E_SIGN",
         "single quote string",
         "double quote string",
         "multi line string",
@@ -71,13 +79,6 @@ static inline void on_word_end(p *parser) {
     }
 }
 
-static inline void on_number_end(p *parser) {
-    size_t pos = parser->word_start;
-    int len = parser->parsed - pos - 1;
-
-    on_token(parser, TLC_TOKEN_INTEGER, pos, len);
-}
-
 static inline void on_string_end(p *parser, tt type) {
     size_t pos = parser->word_start;
     int len = parser->parsed - pos; // include quote
@@ -108,7 +109,7 @@ static inline void on_multi_line_string_end(p *parser) {
 
 static inline void on_long_bracket_end(p *parser) {
     size_t pos = parser->word_start;
-    int len = parser->parsed - pos + parser->long_br_level + 1; // ]===]
+    int len = parser->parsed - pos + parser->bracket_level + 1; // ]===]
 
     on_token(parser, TLC_TOKEN_LONG_BRACKET_STRING, pos, len);
 }
@@ -135,24 +136,6 @@ static inline void on_1_2(p *parser, tt small, uc next, tt big) {
     } else {
         on_1(parser, small);
     }
-}
-
-static inline void on_dot(p *parser) {
-    if (parser->parsed < parser->file->size &&
-        parser->file->content[parser->parsed] == '.'
-    ) {
-        if (parser->parsed + 1 < parser->file->size &&
-            parser->file->content[parser->parsed + 1] == '.'
-        ) {
-            on_3(parser, TLC_TOKEN_ELLIPSIS);
-            return;
-        }
-
-        on_2(parser, TLC_TOKEN_CONCAT);
-        return;
-    }
-
-    on_1(parser, TLC_TOKEN_DOT);
 }
 
 static inline void on_hyphen(p *parser) {
@@ -213,6 +196,48 @@ static inline void on_sq_open(p *parser) {
     on_1(parser, TLC_TOKEN_L_SQ_BR);
 }
 
+static inline void on_number_end(p *parser, tt type) {
+    size_t pos = parser->word_start;
+    int len = parser->parsed - pos - 1;
+
+    on_token(parser, type, pos, len);
+}
+
+static inline void parse_number(
+    p *parser,
+    tlc_parser_num state,
+    int len,
+    tlc_token_type type
+) {
+    parser->word_start = parser->parsed - len;
+    int is_hex = 0;
+state
+type
+}
+
+static inline void on_dot(p *parser) {
+    if (parser->parsed < parser->file->size) {
+        switch (parser->file->content[parser->parsed]) {
+            TLC_CASE_DIGIT:
+                parser->parsed++;
+                parse_number(parser, TLC_PARSER_NUM_DOT, 2, TLC_TOKEN_FLOAT);
+                return;
+            case '.':
+                if (parser->parsed + 1 < parser->file->size &&
+                    parser->file->content[parser->parsed + 1] == '.'
+                ) {
+                    on_3(parser, TLC_TOKEN_ELLIPSIS);
+                    return;
+                }
+
+                on_2(parser, TLC_TOKEN_CONCAT);
+                return;
+        }
+    }
+
+    on_1(parser, TLC_TOKEN_DOT);
+}
+
 static void on_token_start(p *parser, uc c) {
     switch (c) {
         TLC_CASE_ALPHA_LOWER:
@@ -220,8 +245,12 @@ static void on_token_start(p *parser, uc c) {
         case '_':
             on_composite(parser, TLC_PARSER_STATE_WORD);
             return;
-        TLC_CASE_DIGIT:
-            on_composite(parser, TLC_PARSER_STATE_NUMBER);
+        case '0':
+            parse_number(parser, TLC_PARSER_NUM_0, 1, TLC_TOKEN_INTEGER);
+            return;
+        case '1': case '2': case '3': case '4': case '5':
+        case '6': case '7': case '8': case '9':
+            parse_number(parser, TLC_PARSER_NUM_DIG, 1, TLC_TOKEN_INTEGER);
             return;
         case '\'':
             on_composite(parser, TLC_PARSER_STATE_SINGLE_QUOTE_STRING);
@@ -259,21 +288,6 @@ static void on_token_start(p *parser, uc c) {
         case ']': on_1(parser, TLC_TOKEN_R_SQ_BR); return;
         default:
             error_unexpected_symbol(parser, c);
-    }
-}
-
-static inline void on_number(p *parser, uc c) {
-    switch (c) {
-        TLC_CASE_DIGIT:
-        case 'x':
-        case 'X':
-        TLC_CASE_HEX_LOWER:
-        TLC_CASE_HEX_UPPER:
-            return;
-        default:
-            on_number_end(parser);
-            on_token_start(parser, c);
-            return;
     }
 }
 
@@ -357,10 +371,10 @@ static inline void on_multi_line_string(p *parser, uc c) {
 
 static inline void on_long_bracket(p *parser, uc c) {
     if (c == ']' &&
-        parser->parsed + parser->long_br_level < parser->file->size
+        parser->parsed + parser->bracket_level < parser->file->size
     ) {
         on_long_bracket_end(parser);
-        parser->parsed += parser->long_br_level + 1;
+        parser->parsed += parser->bracket_level + 1;
     }
 }
 
@@ -369,7 +383,7 @@ static inline void on_long_bracket_start(p *parser, uc c) {
         case '=':
             return;
         case '[':
-            parser->long_br_level = parser->parsed - parser->word_start - 2;
+            parser->bracket_level = parser->parsed - parser->word_start - 2;
             parser->state = TLC_PARSER_STATE_LONG_BRACKET;
             return;
         default:
@@ -384,9 +398,6 @@ static inline void on_c(p *parser, uc c) {
             break;
         case TLC_PARSER_STATE_WORD:
             on_word(parser, c);
-            break;
-        case TLC_PARSER_STATE_NUMBER:
-            on_number(parser, c);
             break;
         case TLC_PARSER_STATE_SINGLE_QUOTE_STRING:
             on_string_single_quote(parser, c);
@@ -443,10 +454,6 @@ tlc_tokens *tlc_parse_file(tlc_file *file, tlc_config *config) {
         case TLC_PARSER_STATE_WORD:
             parser.parsed++; // emulate space
             on_word_end(&parser);
-            break;
-        case TLC_PARSER_STATE_NUMBER:
-            parser.parsed++; // emulate space
-            on_number_end(&parser);
             break;
         case TLC_PARSER_STATE_SINGLE_LINE_COMMENT:
             parser.parsed++; // emulate \n
--- src/parser.h
+++ src/parser.h
@@ -10,7 +10,6 @@
 typedef enum {
     TLC_PARSER_STATE_TOKEN = 1,
     TLC_PARSER_STATE_WORD,
-    TLC_PARSER_STATE_NUMBER,
     TLC_PARSER_STATE_SINGLE_QUOTE_STRING,
     TLC_PARSER_STATE_DOUBLE_QUOTE_STRING,
     TLC_PARSER_STATE_MULTI_LINE_STRING,
@@ -19,6 +18,20 @@ typedef enum {
     TLC_PARSER_STATE_SINGLE_LINE_COMMENT,
     TLC_PARSER_STATE_MULTI_LINE_COMMENT,
 } tlc_parser_state;
+
+typedef enum {
+    TLC_PARSER_NUM_0 = 1, // 0
+    TLC_PARSER_NUM_DIG, // 1
+    TLC_PARSER_NUM_DOT, // .2  3.
+    TLC_PARSER_NUM_X, // 0x
+    TLC_PARSER_NUM_E,
+    TLC_PARSER_NUM_X_DIG,
+    TLC_PARSER_NUM_X_DOT, // 0x.
+    TLC_PARSER_NUM_X_DIG_DOT, // 0x1.
+    TLC_PARSER_NUM_X_DOT_DIG, // 0x.1
+    TLC_PARSER_NUM_E_DIG,
+    TLC_PARSER_NUM_E_SIGN,
+} tlc_parser_num;
 
 typedef struct {
     tlc_config *config;
@@ -29,7 +42,7 @@ typedef struct {
     size_t line_n;
     size_t line_start;
     size_t word_start;
-    int long_br_level;
+    int bracket_level;
 } tlc_parser;
 
 tlc_tokens *tlc_parse_file(tlc_file *file, tlc_config *config);
